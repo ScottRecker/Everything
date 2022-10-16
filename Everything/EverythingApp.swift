881efe6fdebbf87f7329b5d02f141259e5720034
Scott Recker <scott.a.recker@gmail.com>
2022-10-16 14:33:17 -0500

    Initial Commit

---
+++ Everything/EverythingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverythingApp.swift
+//  Everything
+//
+//  Created by Scott Recker on 10/16/22.
+//
+
+import SwiftUI
+
+@main
+struct EverythingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
